8af4efd99018150a08536c6141f6891322cb3e9a
Wojciech Małota-Wójcik <59281144+wojciech-malota-wojcik@users.noreply.github.com>
2021-11-09 17:30:34 +0100

    Implement COPY operation (#3)

--- app.go
+++ app.go
@@ -28,13 +28,17 @@ func App(ctx context.Context, repo *infra.Repository, builder *infra.Builder) er
 	img := infra.Describe("base",
 		infra.FromScratch(),
 		infra.Label("test", "testValue"),
-		infra.Run("dnf -y install kernel"))
+		infra.Run(`echo "nameserver 8.8.8.8" >> /etc/resolv.conf`),
+		infra.Run(`echo "nameserver 8.8.4.4" >> /etc/resolv.conf`),
+		infra.Copy("/tmp/lala.txt", "test1/test2/lala2.txt"),
+		infra.Run(`dnf -y install kernel`))
 	build, err := builder.Build(ctx, img)
 	if err != nil {
 		return err
 	}
 
-	logger.Get(ctx).Info("Image built", zap.String("path", build.Path()))
+	logger.Get(ctx).Info("Image built", zap.String("path", build.Path()),
+		zap.String("label", build.Label("test")))
 
 	return nil
 }
