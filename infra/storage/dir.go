e28b349a42e2313f74ff359416555f1a40520556
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-18 23:43:04 +0100

    Tag images and assign random build IDs to them

--- infra/storage/dir.go
+++ infra/storage/dir.go
@@ -11,10 +11,10 @@ import (
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/types"
 )
 
-const subDirBuilds = "builds"
-
-// FIXME (wojciech): remove once tags are introduced
-const buildID types.BuildID = "id"
+const (
+	subDirImages = "images"
+	subDirBuilds = "builds"
+)
 
 // NewDirDriver returns new storage driver based on directories
 func NewDirDriver(config runtime.Config) Driver {
@@ -28,16 +28,17 @@ type dirDriver struct {
 }
 
 // Mount mounts image in filesystem
-func (d *dirDriver) Mount(imageName string, buildID types.BuildID, dstPath string) (UnmountFn, error) {
-	srcPath, err := d.toBuildPath(imageName, buildID)
+func (d *dirDriver) Mount(buildID types.BuildID, dstPath string) (UnmountFn, error) {
+	buildLink, err := d.toAbsoluteBuildLink(buildID)
 	if err != nil {
 		return nil, err
 	}
-	if err := os.MkdirAll(srcPath, 0o700); err != nil && !os.IsExist(err) {
+	buildPath, err := filepath.EvalSymlinks(buildLink)
+	if err != nil {
 		return nil, err
 	}
 
-	if err := syscall.Mount(srcPath, dstPath, "", syscall.MS_BIND, ""); err != nil {
+	if err := syscall.Mount(buildPath, dstPath, "", syscall.MS_BIND, ""); err != nil {
 		return nil, err
 	}
 
@@ -46,47 +47,130 @@ func (d *dirDriver) Mount(imageName string, buildID types.BuildID, dstPath strin
 	}, nil
 }
 
+// Create creates blank image
+func (d *dirDriver) Create(imageName string, buildID types.BuildID) error {
+	buildPath, err := d.toAbsoluteBuildDir(imageName, buildID)
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(buildPath, 0o700); err != nil {
+		return err
+	}
+
+	dir := filepath.Join("..", d.toRelativeBuildDir(imageName, buildID))
+	link, err := d.toAbsoluteBuildLink(buildID)
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(link), 0o700); err != nil && !os.IsExist(err) {
+		return err
+	}
+	return os.Symlink(dir, link)
+}
+
 // Clone clones source image to destination or returns false if source image does not exist
-func (d *dirDriver) Clone(srcImageName string, dstImageName string, dstBuildID types.BuildID) error {
-	dstImgPath, err := d.toBuildPath(dstImageName, dstBuildID)
+func (d *dirDriver) Clone(srcImageName string, srcTag types.Tag, dstImageName string, dstBuildID types.BuildID) error {
+	dstImgPath, err := d.toAbsoluteBuildDir(dstImageName, dstBuildID)
 	if err != nil {
 		return err
 	}
 
-	if err := d.Drop(dstImageName, dstBuildID); err != nil {
-		return err
-	}
-
-	srcImgPath, err := d.toBuildPath(srcImageName, buildID)
+	srcImgLink, err := d.toAbsoluteTagLink(srcImageName, srcTag)
 	if err != nil {
 		return err
 	}
-	info, err := os.Stat(srcImgPath)
-	if err != nil && !os.IsNotExist(err) {
+	srcImgPath, err := filepath.EvalSymlinks(srcImgLink)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("image %s:%s does not exist: %w", srcImageName, srcTag, ErrSourceImageDoesNotExist)
+		}
 		return err
 	}
-	if err != nil || !info.IsDir() {
-		return fmt.Errorf("source image %s does not exist: %w", srcImageName, ErrSourceImageDoesNotExist)
+	if err := d.Create(dstImageName, dstBuildID); err != nil {
+		return err
 	}
 	return copy.Copy(srcImgPath, dstImgPath, copy.Options{PreserveTimes: true, PreserveOwner: true})
 }
 
-// Drop drops image
-func (d *dirDriver) Drop(imageName string, buildID types.BuildID) error {
-	imgPath, err := d.toBuildPath(imageName, buildID)
+// Tag tags buildID with tags
+func (d *dirDriver) Tag(buildID types.BuildID, tags []types.Tag) error {
+	buildLink, err := d.toAbsoluteBuildLink(buildID)
 	if err != nil {
 		return err
 	}
-	if err := os.RemoveAll(imgPath); err != nil && !os.IsNotExist(err) {
+	buildDir, err := filepath.EvalSymlinks(buildLink)
+	if err != nil {
 		return err
+	}
+	parentDir := filepath.Dir(buildDir)
+	for _, tag := range tags {
+		tagLink := filepath.Join(parentDir, string(tag))
+	retry:
+		for {
+			err := os.Symlink(string(buildID), tagLink)
+			switch {
+			case err == nil:
+				break retry
+			case os.IsExist(err):
+				if err := os.Remove(tagLink); err != nil && !os.IsNotExist(err) {
+					return err
+				}
+			default:
+				return err
+			}
+		}
 	}
 	return nil
 }
 
-func (d *dirDriver) toBuildPath(imageName string, buildID types.BuildID) (string, error) {
+// Drop drops image
+func (d *dirDriver) Drop(buildID types.BuildID) error {
+	buildLinkPath, err := d.toAbsoluteBuildLink(buildID)
+	if err != nil {
+		return err
+	}
+	imgPath, err := filepath.EvalSymlinks(buildLinkPath)
+	if err != nil {
+		return err
+	}
+
+	if err := os.Remove(buildLinkPath); err != nil {
+		return err
+	}
+
+	if err := os.RemoveAll(imgPath); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	// FIXME (wojciech): remove tags
+
+	return nil
+}
+
+func (d *dirDriver) toRelativeBuildDir(imageName string, buildID types.BuildID) string {
+	return filepath.Join(subDirImages, imageName, string(buildID))
+}
+
+func (d *dirDriver) toAbsoluteBuildDir(imageName string, buildID types.BuildID) (string, error) {
 	rootPath, err := filepath.Abs(d.rootPath)
 	if err != nil {
 		return "", err
 	}
-	return filepath.Join(rootPath, imageName, subDirBuilds, string(buildID)), nil
+	return filepath.Join(rootPath, d.toRelativeBuildDir(imageName, buildID)), nil
+}
+
+func (d *dirDriver) toAbsoluteBuildLink(buildID types.BuildID) (string, error) {
+	rootPath, err := filepath.Abs(d.rootPath)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(rootPath, subDirBuilds, string(buildID)), nil
+}
+
+func (d *dirDriver) toAbsoluteTagLink(imageName string, tag types.Tag) (string, error) {
+	rootPath, err := filepath.Abs(d.rootPath)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(rootPath, subDirImages, imageName, string(tag)), nil
 }
--- infra/types/types.go
+++ infra/types/types.go
@@ -2,3 +2,6 @@ package types
 
 // BuildID is unique ID of build
 type BuildID string
+
+// Tag is the tag of build
+type Tag string
