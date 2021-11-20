10a7eef74c8d33eb96875fd144acfa14b7ad9bee
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-20 22:55:37 +0100

    Introduce formatters

--- commands/list/command.go
+++ commands/list/command.go
@@ -1,14 +1,14 @@
 package list
 
 import (
-	"encoding/json"
 	"fmt"
+	"strings"
 
-	"github.com/ridge/must"
 	"github.com/spf13/cobra"
 	"github.com/wojciech-malota-wojcik/imagebuilder"
 	"github.com/wojciech-malota-wojcik/imagebuilder/commands"
 	"github.com/wojciech-malota-wojcik/imagebuilder/commands/list/config"
+	"github.com/wojciech-malota-wojcik/imagebuilder/infra/format"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/storage"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/types"
 	"github.com/wojciech-malota-wojcik/ioc"
@@ -16,12 +16,21 @@ import (
 
 // Install installs list command
 func Install(c *ioc.Container) {
+	c.Singleton(func() *configFactory {
+		return &configFactory{}
+	})
 	c.Singleton(newConfig)
 	c.TransientNamed("list", command)
 }
 
-func newConfig(args commands.Args) config.List {
+type configFactory struct {
+	// Formatter is the name of formatter to use to convert list into string
+	Formatter string
+}
+
+func newConfig(cf *configFactory, args commands.Args) config.List {
 	config := config.List{
+		Formatter: cf.Formatter,
 		BuildIDs:  make([]types.BuildID, 0, len(args)),
 		BuildKeys: make([]types.BuildKey, 0, len(args)),
 	}
@@ -42,19 +51,21 @@ func newConfig(args commands.Args) config.List {
 	return config
 }
 
-func command(cmdF *commands.CmdFactory) *cobra.Command {
-	return &cobra.Command{
+func command(c *ioc.Container, cf *configFactory, cmdF *commands.CmdFactory) *cobra.Command {
+	cmd := &cobra.Command{
 		Short: "List information about available builds",
-		Use:   "list [...buildID]",
-		RunE: cmdF.Cmd(func(c *ioc.Container) error {
-			var list []storage.BuildInfo
+		Use:   "list [... buildID | [name][:tag]]",
+		RunE: cmdF.Cmd(func(c *ioc.Container, formatter format.Formatter) error {
+			var builds []storage.BuildInfo
 			var err error
-			c.Call(imagebuilder.List, &list, &err)
+			c.Call(imagebuilder.List, &builds, &err)
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(must.Bytes(json.MarshalIndent(list, "", "  "))))
+			fmt.Println(formatter.Format(builds))
 			return nil
 		}),
 	}
+	cmd.Flags().StringVar(&cf.Formatter, "formatter", "json", "Name of formatter used to format the output: "+strings.Join(c.Names((*format.Formatter)(nil)), " | "))
+	return cmd
 }
--- commands/list/config/config.go
+++ commands/list/config/config.go
@@ -6,6 +6,9 @@ import (
 
 // List stores configuration for list command
 type List struct {
+	// Formatter is the name of formatter to use to convert list into string
+	Formatter string
+
 	// BuildIDs is the list of builds to return
 	BuildIDs []types.BuildID
 
