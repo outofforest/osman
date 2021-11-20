dcdb2d2afd424556ae6a60e93c3041cf6536ce84
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-20 14:59:15 +0100

    List build details

--- infra/types/types.go
+++ infra/types/types.go
@@ -5,3 +5,10 @@ type BuildID string
 
 // Tag is the tag of build
 type Tag string
+
+// TagSlice is a sortable representation of slice of tags
+type TagSlice []Tag
+
+func (x TagSlice) Len() int           { return len(x) }
+func (x TagSlice) Less(i, j int) bool { return x[i] < x[j] }
+func (x TagSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
