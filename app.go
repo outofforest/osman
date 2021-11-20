0b2296d377e6c90602d7c2c3acfcab72c23661cd
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-20 12:23:50 +0100

    Use cobra instead of pflag

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/ridge/must"
+	"github.com/spf13/cobra"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/runtime"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/storage"
@@ -19,33 +20,42 @@ import (
 func IoCBuilder(c *ioc.Container) {
 	c.Singleton(runtime.NewConfigFactory)
 	c.Singleton(runtime.NewConfigFromFactory)
+	c.Singleton(runtime.NewCmdFactory)
 	c.Singleton(storage.NewDirDriver)
 	c.Singleton(infra.NewRepository)
 	c.Transient(infra.NewBuilder)
 }
 
 // App runs builder app
-func App(ctx context.Context, config runtime.Config, repo *infra.Repository, builder *infra.Builder) error {
-	if !config.VerboseLogging {
-		logger.VerboseOff()
+func App(cf *runtime.ConfigFactory, cmdF *runtime.CmdFactory) error {
+	rootCmd := &cobra.Command{
+		Short:        "Builds images from spec files",
+		SilenceUsage: true,
+		RunE: cmdF.Cmd(func(ctx context.Context, config runtime.Config, repo *infra.Repository, builder *infra.Builder) error {
+			fedoraCmds := []infra.Command{infra.Run(`printf "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > /etc/resolv.conf`),
+				infra.Run(`echo 'LANG="en_US.UTF-8"' > /etc/locale.conf`),
+				infra.Run(`rm -rf /var/cache/* /tmp/*`)}
+
+			repo.Store(infra.Describe("fedora", []types.Tag{"34"}, fedoraCmds...))
+			repo.Store(infra.Describe("fedora", []types.Tag{"35"}, fedoraCmds...))
+
+			for i, specFile := range config.SpecFiles {
+				must.OK(os.Chdir(filepath.Dir(specFile)))
+
+				build, err := builder.BuildFromFile(ctx, specFile, config.Names[i], config.Tags...)
+				if err != nil {
+					return err
+				}
+				logger.Get(ctx).Info("Image built", zap.Strings("params", build.Manifest().Params))
+			}
+			return nil
+		}),
 	}
 
-	fedoraCmds := []infra.Command{infra.Run(`printf "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > /etc/resolv.conf`),
-		infra.Run(`echo 'LANG="en_US.UTF-8"' > /etc/locale.conf`),
-		infra.Run(`rm -rf /var/cache/* /tmp/*`)}
-
-	repo.Store(infra.Describe("fedora", []types.Tag{"34"}, fedoraCmds...))
-	repo.Store(infra.Describe("fedora", []types.Tag{"35"}, fedoraCmds...))
-
-	for i, specFile := range config.SpecFiles {
-		must.OK(os.Chdir(filepath.Dir(specFile)))
-
-		build, err := builder.BuildFromFile(ctx, specFile, config.Names[i], config.Tags...)
-		if err != nil {
-			return err
-		}
-		logger.Get(ctx).Info("Image built", zap.Strings("params", build.Manifest().Params))
-	}
-
-	return nil
+	rootCmd.Flags().StringSliceVar(&cf.Names, "name", []string{}, "Name of built image, if empty name is derived from corresponding specfile")
+	rootCmd.Flags().StringSliceVar(&cf.Tags, "tag", []string{string(runtime.DefaultTag)}, "Tags assigned to created build")
+	rootCmd.Flags().BoolVar(&cf.Rebuild, "rebuild", false, "If set, all parent images are rebuilt even if they exist")
+	rootCmd.PersistentFlags().StringVar(&cf.RootDir, "root-dir", filepath.Join(must.String(os.UserHomeDir()), ".images"), "Directory where built images are stored")
+	rootCmd.PersistentFlags().BoolVarP(&cf.VerboseLogging, "verbose", "v", false, "Turns on verbose logging")
+	return rootCmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,19 @@ go 1.16
 
 replace github.com/ridge/parallel => github.com/wojciech-malota-wojcik/parallel v0.1.2
 
+// rsc.io is unreliable, take it from github
+replace (
+	rsc.io/binaryregexp => github.com/rsc/binaryregexp v0.2.0
+	rsc.io/quote/v3 => github.com/rsc/quote/v3 v3.1.0
+	rsc.io/sampler => github.com/rsc/sampler v1.3.1
+)
+
 require (
 	github.com/google/uuid v1.3.0
 	github.com/otiai10/copy v1.7.0
 	github.com/pkg/errors v0.8.1
 	github.com/ridge/must v0.6.0
+	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
 	github.com/wojciech-malota-wojcik/build v0.0.0-20210131144749-3ef5b00b908f
 	github.com/wojciech-malota-wojcik/buildgo v0.1.1
