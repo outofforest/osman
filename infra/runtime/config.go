59c64656866ed4359f2380d7f358de66fdf60533
Wojciech Małota-Wójcik <59281144+wojciech-malota-wojcik@users.noreply.github.com>
2021-11-20 13:34:08 +0100

    Forgot to remove reading flags with pflag (#32)

--- infra/runtime/config.go
+++ infra/runtime/config.go
@@ -1,12 +1,9 @@
 package runtime
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/ridge/must"
-	"github.com/spf13/pflag"
 	"github.com/wojciech-malota-wojcik/imagebuilder/infra/types"
 )
 
@@ -15,20 +12,16 @@ const DefaultTag types.Tag = "latest"
 
 // NewConfigFactory creates new config factory
 func NewConfigFactory() *ConfigFactory {
-	cf := &ConfigFactory{}
-	pflag.StringVar(&cf.RootDir, "root-dir", filepath.Join(must.String(os.UserHomeDir()), ".images"), "Directory where built images are stored")
-	pflag.StringSliceVar(&cf.Names, "name", []string{}, "Name of built image, if empty name is derived from corresponding specfile")
-	pflag.StringSliceVar(&cf.Tags, "tag", []string{string(DefaultTag)}, "Tags assigned to created build")
-	pflag.BoolVar(&cf.Rebuild, "rebuild", false, "If set, all parent images are rebuilt even if they exist")
-	pflag.BoolVarP(&cf.VerboseLogging, "verbose", "v", false, "Turns on verbose logging")
-	pflag.Parse()
-	return cf
+	return &ConfigFactory{}
 }
 
 // ConfigFactory produces config from parameters
 type ConfigFactory struct {
 	// RootDir is the root directory for images
 	RootDir string
+
+	// SpecFiles is the list of specfiles to build
+	SpecFiles []string
 
 	// Names is the list of names for corresponding specfiles
 	Names []string
@@ -47,16 +40,15 @@ type ConfigFactory struct {
 func NewConfigFromFactory(cf *ConfigFactory) Config {
 	config := Config{
 		RootDir:        cf.RootDir,
-		SpecFiles:      make([]string, 0, pflag.NArg()),
+		SpecFiles:      cf.SpecFiles,
 		Names:          cf.Names,
 		Tags:           make([]types.Tag, 0, len(cf.Tags)),
 		Rebuild:        cf.Rebuild,
 		VerboseLogging: cf.VerboseLogging,
 	}
 
-	for i := 0; i < pflag.NArg(); i++ {
-		specFile := pflag.Arg(i)
-		config.SpecFiles = append(config.SpecFiles, specFile)
+	config.SpecFiles = cf.SpecFiles
+	for i, specFile := range config.SpecFiles {
 		if len(config.Names) < i+1 {
 			config.Names = append(config.Names, strings.TrimSuffix(filepath.Base(specFile), ".spec"))
 		}
