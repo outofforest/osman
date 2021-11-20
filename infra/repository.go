c05e0af942a5e59000e40bcf690bf81b514ac5c1
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-20 09:34:09 +0100

    Prevent loops in dependencies

--- infra/repository.go
+++ infra/repository.go
@@ -1,32 +1,29 @@
 package infra
 
-import "github.com/wojciech-malota-wojcik/imagebuilder/infra/types"
-
-type repoKey struct {
-	name string
-	tag  types.Tag
-}
+import (
+	"github.com/wojciech-malota-wojcik/imagebuilder/infra/types"
+)
 
 // NewRepository creates new image repository
 func NewRepository() *Repository {
 	return &Repository{
-		images: map[repoKey]*Descriptor{},
+		images: map[buildKey]*Descriptor{},
 	}
 }
 
 // Repository is an image repository
 type Repository struct {
-	images map[repoKey]*Descriptor
+	images map[buildKey]*Descriptor
 }
 
 // Store stores image descriptor in repository
 func (r *Repository) Store(img *Descriptor) {
 	for _, tag := range img.Tags() {
-		r.images[repoKey{name: img.Name(), tag: tag}] = img
+		r.images[buildKey{name: img.Name(), tag: tag}] = img
 	}
 }
 
 // Retrieve retrieves image descriptor from repository
 func (r *Repository) Retrieve(name string, tag types.Tag) *Descriptor {
-	return r.images[repoKey{name: name, tag: tag}]
+	return r.images[buildKey{name: name, tag: tag}]
 }
