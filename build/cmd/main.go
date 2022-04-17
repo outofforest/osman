d4371ba9253759c7e469bf2335be736bbb4e447c
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-04-17 21:47:34 +0200

    Use new build system

--- build/cmd/main.go
+++ build/cmd/main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"context"
-	"os"
-	"path/filepath"
 
 	"github.com/outofforest/build"
 	"github.com/outofforest/ioc/v2"
 	"github.com/outofforest/run"
-	"github.com/ridge/must"
 
 	me "github.com/outofforest/osman/build"
 )
 
 func main() {
 	run.Tool("build", nil, func(ctx context.Context, c *ioc.Container) error {
-		exec := build.NewIoCExecutor(me.Commands, c)
-		if build.Autocomplete(exec) {
-			return nil
-		}
-
-		changeWorkingDir()
-		return build.Do(ctx, "go", exec)
+		return build.Do(ctx, "go-env-v1", build.NewIoCExecutor(me.Commands, c))
 	})
-}
-
-func changeWorkingDir() {
-	must.OK(os.Chdir(filepath.Dir(filepath.Dir(must.String(filepath.EvalSymlinks(must.String(os.Executable())))))))
 }
