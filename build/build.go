bafb122caf9eb68af024c9b066c6f7a5fe9c4c07
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2022-04-18 21:19:29 +0200

    Upgrade libraries (#115)

--- build/build.go
+++ build/build.go
@@ -1,17 +1,10 @@
 package build
 
 import (
-	"context"
-
 	"github.com/outofforest/build"
 	"github.com/outofforest/buildgo"
 )
 
 func setup(deps build.DepsFunc) {
 	deps(buildgo.InstallAll)
-}
-
-func buildMe(ctx context.Context, deps build.DepsFunc) error {
-	deps(buildgo.EnsureGo)
-	return buildgo.GoBuildPkg(ctx, "build/cmd", "bin/tmp-osman", true)
 }
