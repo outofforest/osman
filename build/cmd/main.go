33867a1281194f2ca86958379e09532b41fb71fd
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2022-05-07 19:16:04 +0200

    Upgrade `build` (#131)

--- build/cmd/main.go
+++ build/cmd/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"context"
-
 	"github.com/outofforest/build"
-	"github.com/outofforest/ioc/v2"
-	"github.com/outofforest/run"
 
 	me "build"
 )
 
 func main() {
-	run.Tool("build", nil, func(ctx context.Context, c *ioc.Container) error {
-		return build.Do(ctx, "go-env-v1", build.NewIoCExecutor(me.Commands, c))
-	})
+	build.Main("go-env-v1", nil, me.Commands)
 }
