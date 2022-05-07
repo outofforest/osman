bada148d3fa06a5cd533c76f70e5d92f0df313c8
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-05-07 19:14:22 +0200

    Upgrade `build`

--- build/index.go
+++ build/index.go
@@ -1,12 +1,15 @@
 package build
 
-import "github.com/outofforest/buildgo"
+import (
+	"github.com/outofforest/build"
+	"github.com/outofforest/buildgo"
+)
 
 // Commands is a definition of commands available in build system
-var Commands = map[string]interface{}{
-	"setup": setup,
-	"build": buildApp,
-	"run":   runApp,
+var Commands = map[string]build.Command{
+	"setup": {Fn: setup, Description: "Installs tools required by development environment"},
+	"build": {Fn: buildApp, Description: "Builds osman binary"},
+	"run":   {Fn: runApp, Description: "Builds and runs osman binary"},
 }
 
 func init() {
