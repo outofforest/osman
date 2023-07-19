e187ff3670ba1132a99c93044f843d091e92bda1
Wojciech Malota-Wojcik <wojtek@exw.co>
2023-07-19 19:23:23 +0200

    Use new design of `isolator`

--- infra/base/docker.go
+++ infra/base/docker.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/outofforest/isolator"
 	"github.com/outofforest/isolator/wire"
-	"github.com/outofforest/parallel"
 	"github.com/pkg/errors"
 
 	"github.com/outofforest/osman/infra/types"
@@ -22,61 +21,50 @@ type dockerInitializer struct {
 }
 
 // Init fetches image from docker registry and integrates it inside directory
-func (f *dockerInitializer) Init(ctx context.Context, cacheDir, dir string, buildKey types.BuildKey) error {
-	return parallel.Run(ctx, func(ctx context.Context, spawn parallel.SpawnFn) error {
-		incoming := make(chan interface{})
-		outgoing := make(chan interface{})
+func (f *dockerInitializer) Init(ctx context.Context, cacheDir, dir string, buildKey types.BuildKey) (retErr error) {
+	cacheDir = filepath.Join(cacheDir, "docker-images")
+	if err := os.MkdirAll(cacheDir, 0o700); err != nil {
+		return errors.WithStack(err)
+	}
 
-		cacheDir := filepath.Join(cacheDir, "docker-images")
-		if err := os.MkdirAll(cacheDir, 0o700); err != nil {
-			return errors.WithStack(err)
+	return isolator.Run(ctx, isolator.Config{
+		Dir: dir,
+		Types: []interface{}{
+			wire.Result{},
+		},
+		Executor: wire.Config{
+			UseHostNetwork: true,
+			Mounts: []wire.Mount{
+				{
+					Host:      cacheDir,
+					Namespace: "/.docker-cache",
+					Writable:  true,
+				},
+			},
+		},
+	}, func(ctx context.Context, incoming <-chan interface{}, outgoing chan<- interface{}) error {
+		select {
+		case <-ctx.Done():
+			return errors.WithStack(ctx.Err())
+		case outgoing <- wire.InflateDockerImage{
+			CacheDir: "/.docker-cache",
+			Image:    buildKey.Name,
+			Tag:      string(buildKey.Tag),
+		}:
 		}
 
-		spawn("isolator", parallel.Fail, func(ctx context.Context) error {
-			return isolator.Run(ctx, isolator.Config{
-				Dir: dir,
-				Types: []interface{}{
-					wire.Result{},
-				},
-				Executor: wire.Config{
-					NoStandardMounts: true,
-					Mounts: []wire.Mount{
-						{
-							Host:      cacheDir,
-							Container: "/.docker-cache",
-							Writable:  true,
-						},
-					},
-				},
-				Incoming: incoming,
-				Outgoing: outgoing,
-			})
-		})
-		spawn("init", parallel.Exit, func(ctx context.Context) error {
-			select {
-			case <-ctx.Done():
-				return errors.WithStack(ctx.Err())
-			case outgoing <- wire.InflateDockerImage{
-				CacheDir: "/.docker-cache",
-				Image:    buildKey.Name,
-				Tag:      string(buildKey.Tag),
-			}:
-			}
-
-			select {
-			case <-ctx.Done():
-				return errors.WithStack(ctx.Err())
-			case content := <-incoming:
-				result, ok := content.(wire.Result)
-				if !ok {
-					return errors.Errorf("expected Result, got: %T", content)
-				}
-				if result.Error != "" {
-					return errors.New(result.Error)
+		for content := range incoming {
+			switch m := content.(type) {
+			case wire.Result:
+				if m.Error != "" {
+					return errors.New(m.Error)
 				}
 				return nil
+			default:
+				return errors.New("unexpected message received")
 			}
-		})
-		return nil
+		}
+
+		return errors.WithStack(ctx.Err())
 	})
 }
